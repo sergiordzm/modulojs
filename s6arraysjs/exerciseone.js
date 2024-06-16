fc151b8d84767b0e0a5dbf62c20719047904d1c2
Sergio <smrma1308@gmail.com>
2024-06-16 15:22:48 -0600

    array exercise

--- s6arraysjs/exerciseone.js
+++ s6arraysjs/exerciseone.js
@@ -1,76 +1,3 @@
-let users = [
-  {
-    name: "Juan",
-    lastname: "Pérez",
-    age: 25,
-    hobbies: ["leer", "correr"],
-    country: "México",
-  },
-  {
-    name: "Ana",
-    lastname: "García",
-    age: 30,
-    hobbies: ["viajar", "pintar", "nadar"],
-    country: "Argentina",
-  },
-  {
-    name: "Carlos",
-    lastname: "López",
-    age: 45,
-    hobbies: ["cantar"],
-    country: "Chile",
-  },
-  {
-    name: "María",
-    lastname: "Fernández",
-    age: 22,
-    hobbies: ["leer", "correr", "bailar"],
-    country: "Colombia",
-  },
-  {
-    name: "Luis",
-    lastname: "Gómez",
-    age: 35,
-    hobbies: ["jugar fútbol", "viajar"],
-    country: "México",
-  },
-  {
-    name: "Sofía",
-    lastname: "Rodríguez",
-    age: 28,
-    hobbies: ["cocinar", "leer"],
-    country: "Chile",
-  },
-  {
-    name: "José",
-    lastname: "Sánchez",
-    age: 40,
-    hobbies: ["nadar", "escribir"],
-    country: "Argentina",
-  },
-  {
-    name: "Laura",
-    lastname: "Martínez",
-    age: 26,
-    hobbies: ["pintar", "cantar"],
-    country: "México",
-  },
-  {
-    name: "Miguel",
-    lastname: "Hernández",
-    age: 33,
-    hobbies: ["correr", "jugar fútbol"],
-    country: "Colombia",
-  },
-  {
-    name: "Lucía",
-    lastname: "Torres",
-    age: 29,
-    hobbies: ["viajar", "bailar"],
-    country: "Chile",
-  },
-];
-
 /*
   con el array numbers:
     1.- Obtener la suma total de todos los números que estan dentro del array
@@ -164,3 +91,76 @@ console.log(resultFulllName);
     4.- Necesito obtener una nueva lista con base en un hobbie ( includes )
     5.- Necesito una lista que contenga los diferentes hobbies de nuestros usuarios sin repetir
 */
+
+let users = [
+  {
+    name: "Juan",
+    lastname: "Pérez",
+    age: 25,
+    hobbies: ["leer", "correr"],
+    country: "México",
+  },
+  {
+    name: "Ana",
+    lastname: "García",
+    age: 30,
+    hobbies: ["viajar", "pintar", "nadar"],
+    country: "Argentina",
+  },
+  {
+    name: "Carlos",
+    lastname: "López",
+    age: 45,
+    hobbies: ["cantar"],
+    country: "Chile",
+  },
+  {
+    name: "María",
+    lastname: "Fernández",
+    age: 22,
+    hobbies: ["leer", "correr", "bailar"],
+    country: "Colombia",
+  },
+  {
+    name: "Luis",
+    lastname: "Gómez",
+    age: 35,
+    hobbies: ["jugar fútbol", "viajar"],
+    country: "México",
+  },
+  {
+    name: "Sofía",
+    lastname: "Rodríguez",
+    age: 28,
+    hobbies: ["cocinar", "leer"],
+    country: "Chile",
+  },
+  {
+    name: "José",
+    lastname: "Sánchez",
+    age: 40,
+    hobbies: ["nadar", "escribir"],
+    country: "Argentina",
+  },
+  {
+    name: "Laura",
+    lastname: "Martínez",
+    age: 26,
+    hobbies: ["pintar", "cantar"],
+    country: "México",
+  },
+  {
+    name: "Miguel",
+    lastname: "Hernández",
+    age: 33,
+    hobbies: ["correr", "jugar fútbol"],
+    country: "Colombia",
+  },
+  {
+    name: "Lucía",
+    lastname: "Torres",
+    age: 29,
+    hobbies: ["viajar", "bailar"],
+    country: "Chile",
+  },
+];
