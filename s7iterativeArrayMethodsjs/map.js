176d90d8c179eaaa3a1ffb035cca9a660eabdcc7
Sergio <smrma1308@gmail.com>
2024-06-18 00:36:04 -0600

    map method

---
+++ s7iterativeArrayMethodsjs/map.js
@@ -0,0 +1,2 @@
+/*.map()*/
+/*Método iterativo de array, itera en un array, ejecuta un callback por cada elemento del array, y devuelve un nuevo array, con LA MISMA CANTIDAD DE ELEMENTOS QUE EL ARRAY ORIGINAL. Cada uno de los elementos del nuevo array será el resultado del valor de retorno del callback*/
